f9a42e5184f72bfda9d6d8866c078c1598d8fb55
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-15 14:44:52 +0700

    Update JS

--- JS/Index.js
+++ JS/Index.js
@@ -1,14 +1,15 @@
-var message0 = "Hello Coder";
-let message1 = "Hello World";
-
-const message2 = "Hello CodeWar";
+var message0 = "Riga";
+let message1 = "Fujiwara";
+message0 = "Dark"
+const message3 = "19";
 
 if(true){
     var message0 = 1;
-    let message2 = 2;
+    let message1 = 2;
 
-    console.log("TEST", message2)
+    console.log("TEST", message1)
 }
 
 console.log(message0)
 console.log(message1)
+console.log(message3)
