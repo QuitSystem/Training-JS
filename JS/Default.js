346bc1deb579546052109094f9e2c2619f3317be
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-15 16:50:52 +0700

    Add Default

---
+++ JS/Default.js
@@ -0,0 +1,6 @@
+function DropData(x = 100) {
+    console.log(x);
+}
+
+DropData()
+// Jika value di isi maka itu lah yang akan keluar berbeda jika dikosongkan maka yang keluar akan value dari Function //    
