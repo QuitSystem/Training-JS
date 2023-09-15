9ac8ad6296d2f453b86002f975e71a4a218e9203
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-15 15:07:56 +0700

    Update JS & New JS File

---
+++ JS/Boolean.js
@@ -0,0 +1,10 @@
+var tinggi = false;
+var km = 1001;
+// Kalau value dari km lebih kecil value if maka yang keluar akan false //
+// Dan berbeda jika value km lebih besar dari if maka yang keluar akan true // 
+if(km>1000)
+tinggi = true;
+else
+tinggi = false;
+
+console.log(tinggi)
