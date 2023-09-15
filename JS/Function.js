63182999b183f34a35b0f38c60651d2891daefe5
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-15 16:13:47 +0700

    Add Function

---
+++ JS/Function.js
@@ -0,0 +1,20 @@
+function Roadone() {
+    console.log("Roadone")
+}
+
+const Roadtwo = function() {
+    console.log("Roadtwo")
+}
+
+// Kedua Function di atas ini adalah penulisan Function lama //
+
+Roadone()
+Roadtwo()
+
+const Roadthree = ()=>{
+    console.log("Roadthree")
+}
+
+// Sedangkan untuk Function yang yang ke-3 adalah penulisan Function baru //
+
+Roadthree()
