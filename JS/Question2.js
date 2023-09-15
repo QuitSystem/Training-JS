3e4bf942c9bcda9658e28e08527357ef50e51622
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-15 15:39:27 +0700

    Update JS

---
+++ JS/Question2.js
@@ -0,0 +1,40 @@
+// Datanumber //
+var number1 = 100;
+var number2 = 120.5;
+var number3 = -100;
+
+console.log(number1)
+console.log(number2)
+console.log(number3)
+
+// String //
+var huruf = "abcde";
+var tunggal = "tunggal";
+
+console.log(huruf)
+console.log(tunggal)
+
+// Boolean //
+var tinggi = false;
+var km = 1001;
+// Kalau value dari km lebih kecil value if maka yang keluar akan false //
+// Dan berbeda jika value km lebih besar dari if maka yang keluar akan true // 
+if(km>1000)
+tinggi = true;
+else
+tinggi = false;
+
+console.log(tinggi)
+
+// Daranull //
+var kosong = "ada";
+// Jika var tidak diberi value akan menghasilkan undifined //
+kosong = null;
+// berbeda jika null karena null ada sebuah value //
+console.log(kosong)
+
+// Array //
+var angka = [3,4,5,6,7];
+
+console.log(angka[0])
+
