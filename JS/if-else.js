ea9f8d8add97a7db42bc33600d6bf6a92ed276ea
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 15:22:47 +0700

    update if-else

--- JS/if-else.js
+++ JS/if-else.js
@@ -1,8 +1,12 @@
-function Road1() {
-    var nilai = 3;
-    if(nilai === 3) {
-        console.log("OKE")
+function Test1() {
+    var nilai = 4;
+    if(nilai === 10) {
+        console.log("GOOD")
+    } else if(nilai === 5){
+        console.log("BETTER")
     } else {
-        console.log("NO")
+        console.log("NOGOOD")
     }
 }
+
+Test1()
