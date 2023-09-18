61a8e02265e0ce6e9c75785c2a4d518c6bb79275
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 15:02:39 +0700

    Update Question3

--- JS/Question/Question3.js
+++ JS/Question/Question3.js
@@ -1,16 +1,9 @@
-function genggam() {
+function jatuhke() {
 
-    return "lempar";
+return "naik";
+
+return
+
 }
-//  Id tidak berfungsi //
-var tampung = genggam();
-console.log(tampung);
-
-function AddData(a,b,c) {
-
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-AddData("Black","Gray","White")
+var atas = jatuhke();
+console.log(atas);
