3edf590339aeca876814668335bf84557becf1d2
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 17:35:37 +0700

    Add infinity

---
+++ JS/Infinity.js
@@ -0,0 +1,13 @@
+for(var i = 0; i > 0; i++){
+    console.log(i)
+}
+
+// contoh diatas adalah yang menggunakan for //
+
+var i = 1;
+while(true){
+    console.log(i)
+    i++;
+}
+
+//  dan ini jika menggunakan while //
