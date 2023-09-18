917f38405f8e7f8f0479cd3e921a61f1122866a5
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 16:43:34 +0700

    Add switch

---
+++ JS/switch.js
@@ -0,0 +1,17 @@
+function testingSwitch() {
+  var nilai = 10;
+
+  switch (nilai) {
+    case "10":
+      console.log("sangat bagus")
+      break;
+    case "8":
+      console.log("bagus")
+      break;
+    case "10":
+      console.log("baik")
+      break;
+    default:
+      console.log("cukup")
+  }
+}
