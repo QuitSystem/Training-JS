92b506e9aa3257552233583fc2e8d1cf0144111d
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 16:28:13 +0700

    Update Question3

--- JS/Question/Question3.js
+++ JS/Question/Question3.js
@@ -1,9 +1,15 @@
-function jatuhke() {
-
-return "naik";
-
-return
-
+// Fungsi yang menerima nilai, mengubahnya, dan mengembalikan nilai yang telah diubah
+function ubahNilai(nilai) {
+  nilai *= 2;
+  return nilai;
 }
-var atas = jatuhke();
-console.log(atas);
+
+// Nilai awal sebelum pengubahan
+let nilaiAwal = 5;
+console.log("Nilai awal:", nilaiAwal);
+
+// Memanggil fungsi untuk mengubah nilai
+let nilaiSetelahPerubahan = ubahNilai(nilaiAwal);
+
+// Menunjukkan nilai setelah pengubahan
+console.log("Nilai setelah perubahan:", nilaiSetelahPerubahan);
