fadaefc75dc3ae6bd800f425517a38c2a59c4027
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 18:43:10 +0700

    Add Nested-array

---
+++ JS/Nested-array.js
@@ -0,0 +1,10 @@
+function panggilNestedArray(){
+    var namaData = [["Ab","Ac","Ad",],
+                    ["Road1","Road2","Road3"],
+                    ["Home1","Home2","Home3"]]
+
+    console.log(namaData.length)
+    console.log(namaData[2][2])
+}
+
+panggilNestedArray()
