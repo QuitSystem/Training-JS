93223caac813868a31d52b41549681e0b2dcc8ff
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 16:59:18 +0700

    Add Question4

---
+++ JS/Question/Question4.js
@@ -0,0 +1,10 @@
+var x = 180;
+
+if (x === 200)
+console.log("Sangat tinggi")
+else if (x < 200)
+console.log("Lumayan tinngi")
+else if (x === 180)
+console.log("Rata - rata")
+else
+console.log("Dibawah rata - rata")
