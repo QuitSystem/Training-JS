0ae72bf80f0dc8310142659255a103f52b921494
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-18 15:33:04 +0700

    Add if-elseif

---
+++ JS/if-elseif.js
@@ -0,0 +1,10 @@
+var x = 100;
+
+if (x>150)
+console.log("x lebih besar dari 150")
+else if (x>100)
+console.log("x lebih besar dari 100")
+else if (x<100)
+console.log("x lebih kecil dari 100")
+else
+console.log("x sama dengan 100")
