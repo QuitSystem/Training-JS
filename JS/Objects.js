ad12ef4ce53653ca579165d623ba954fdf0a336e
Fahrul Fauzi <dnxrd01gmail.com>
2023-09-19 15:22:39 +0700

    Add objects

---
+++ JS/Objects.js
@@ -0,0 +1,15 @@
+function panggilObjects() {
+  var mobil = {
+    type: "sport",
+    price: 2000000000,
+    color: "black",
+  };
+
+  console.log(mobil)
+  console.log(mobil.type)
+  console.log(mobil.price)
+  console.log(mobil.color)
+}
+
+
+panggilObjects()
