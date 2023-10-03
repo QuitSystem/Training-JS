161f31e7d1cb6c290ccd2736d4c15088db15a6c9
RIGX <dnxrd01gmail.com>
2023-10-03 15:49:43 +0700

    Add Operation

---
+++ JS/Operation.js
@@ -0,0 +1,14 @@
+function Operation() {
+    var a = 5;
+    var b = 5;
+    var c = 5;
+    var d = 5;
+    var e = 5;
+
+    return(a+b)
+    return(a-c)
+    return(a*d)
+    return(a/e)
+}
+
+console.log(Operation())
