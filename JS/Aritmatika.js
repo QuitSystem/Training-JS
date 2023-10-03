d8fe419ce02ccd392a303ba30de9f11007c9d220
RIGX <dnxrd01gmail.com>
2023-10-03 15:58:14 +0700

    Add Aritmatika

---
+++ JS/Aritmatika.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 10;
+var c = 15;
+var d = 20;
+var e = 25;
+
+console.log(a+b) //Tambah
+console.log(a-c) //Kurang
+console.log(a*d) //Kali
+console.log(a/e) //Bagi
+console.log(a%a) //Sisa Bagi
